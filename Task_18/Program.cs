07590cd0e4c75a7324d13d6020e6419586c7bc77
Margarita <margarita_anis@bk.ru>
2022-05-18 20:49:02 +0300

    Add project Task_18

---
+++ Task_18/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа по заданному номеру четверти показывает диапазон возможных координат 
+// точек в этой четверти (X и Y)
+
+Console.Write("Введите четверть диапазона: ");
+int num = int.Parse(Console.ReadLine());
+
+if (num == 1)
+{
+    Console.WriteLine("Диапазон координат: x > 0 и y > 0");
+}
+else if (num == 2)
+{
+    Console.WriteLine("Диапазон координат: x < 0 и y > 0 ");
+}
+else if (num == 3)
+{
+    Console.WriteLine("Диапазон координат: x < 0 и y < 0");
+}
+else if (num == 4)
+{
+    Console.WriteLine("Диапазон координат: x > 0 и y < 0");
+}
+else Console.WriteLine("Введены невенрные данные");
