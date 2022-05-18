1195e4be71f0719b798919d7fab1b7d56222a2ec
Margarita <margarita_anis@bk.ru>
2022-05-18 22:09:43 +0300

    Add project Task_22

---
+++ Task_22/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа на вход принимает число (N) и выдает таблицу квадратов чисел от 1 до N
+
+Console.Write("Введите число -> ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= n) 
+{
+    int res = i * i;
+    Console.WriteLine($" Число {i, 5}| Квадрат числа {res, 5}" );
+    i++;
+}
