c0f829c6b649c8c9391da1ca66119eed6ed5718c
Margarita <margarita_anis@bk.ru>
2022-05-19 19:58:41 +0300

    Add HOMEWORK project Task_20

---
+++ Task_20/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа на вход принимает координаты двух точек и находит расстояние
+// между ними в 3D пространстве
+
+Console.WriteLine("Введите координаты первой точки: ");
+double x1 = double.Parse(Console.ReadLine());
+double y1 = double.Parse(Console.ReadLine());
+double z1 = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты второй точки: ");
+double x2 = double.Parse(Console.ReadLine());
+double y2 = double.Parse(Console.ReadLine());
+double z2 = double.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1) + (z2 - z1) * (z2 - z1));
+double n = Math.Round(distance, 2);
+Console.WriteLine($"Расстояние между точками в 3D пространстве: {n}");
