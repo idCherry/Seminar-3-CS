401b38e813eacd3bbbdada9ae264f4a8a2305496
Margarita <margarita_anis@bk.ru>
2022-05-19 20:12:08 +0300

    Add HOMEWORK project Task_23

---
+++ Task_23/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N
+
+
+Console.Write("Введите число -> ");
+int n = int.Parse(Console.ReadLine());
+int i = 1;
+
+while (i <= n) 
+{
+    int res = i * i * i;
+    Console.WriteLine($" Число {i, 7} | Куб числа {res, 7}" );
+    i++;
+}
+
