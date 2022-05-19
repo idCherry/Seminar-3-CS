84e5bd6a54019e20bafd9653355f03180654214b
Margarita <margarita_anis@bk.ru>
2022-05-19 20:38:28 +0300

    Add HOMEWORK project Task_19

---
+++ Task_19/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа принимает на вход 5-значное число и проверяет
+// является ли оно полиндромом
+
+Console.Write("Введите пятизначное число: ");
+int num = int.Parse(Console.ReadLine());
+int num1 = num / 10000;
+int num2 = (num / 1000) % 10;
+int num4 = (num / 10) % 10;
+int num5 = num % 10;
+if (num1 == num5 && num2 == num4) Console.WriteLine("Число является полиндромом");
+else Console.WriteLine("Число не является полиндромом");
+
+
+
